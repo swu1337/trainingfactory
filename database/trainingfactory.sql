de879412dddb2330145cd1cef665ed69df7dd1cc
Wouter van Kester <wouter_k_@hotmail.com>
2017-03-31 12:07:24 +0200

    adding database

--- database/trainingfactory.sql
+++ database/trainingfactory.sql
@@ -1,1 +1,131 @@
-/*Database*/
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 31 mrt 2017 om 10:02
+-- Serverversie: 5.7.14
+-- PHP-versie: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `training_factory`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `lessons`
+--
+
+CREATE TABLE `lessons` (
+  `id` int(11) NOT NULL,
+  `time` time NOT NULL,
+  `date` date NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `max_persons` int(11) NOT NULL,
+  `instructeur_id` int(11) NOT NULL,
+  `training_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `persons`
+--
+
+CREATE TABLE `persons` (
+  `id` int(11) NOT NULL,
+  `loginname` varchar(20) NOT NULL,
+  `password` varchar(65) NOT NULL DEFAULT 'qwerty',
+  `firstname` varchar(25) NOT NULL,
+  `preprovision` varchar(10) NOT NULL,
+  `lastname` varchar(35) NOT NULL,
+  `dateofbirth` date NOT NULL,
+  `gender` enum('male','female','other') NOT NULL,
+  `email_address` varchar(100) NOT NULL,
+  `hiring_date` date NOT NULL,
+  `salary` decimal(7,0) NOT NULL,
+  `street` varchar(50) NOT NULL,
+  `postal_code` varchar(10) NOT NULL,
+  `place` varchar(35) NOT NULL,
+  `role` enum('instructeur','lid') NOT NULL DEFAULT 'lid'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `registrations`
+--
+
+CREATE TABLE `registrations` (
+  `lesson_id` int(11) NOT NULL,
+  `member_id` int(11) NOT NULL,
+  `payment` varchar(30) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `trainings`
+--
+
+CREATE TABLE `trainings` (
+  `id` int(11) NOT NULL,
+  `discription` varchar(255) NOT NULL,
+  `duration` time NOT NULL,
+  `extra_costs` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `lessons`
+--
+ALTER TABLE `lessons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `persons`
+--
+ALTER TABLE `persons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `trainings`
+--
+ALTER TABLE `trainings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `lessons`
+--
+ALTER TABLE `lessons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT voor een tabel `persons`
+--
+ALTER TABLE `persons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT voor een tabel `trainings`
+--
+ALTER TABLE `trainings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
