bf52d49382baf2f68c9a990bae02f1f3a4c2a04c
GeertHeemskerk <leaguecrafterhd@gmail.com>
2017-04-05 11:48:44 +0200

    Made the footer

--- dev/view/templates/includes/footer.php
+++ dev/view/templates/includes/footer.php
@@ -1,6 +1,7 @@
         </section>
         <footer>
-            &copy;Training Centrum Den Haag 010 519 0532, Developed by FastDevelopment 2017
+            <article class="footer-text">&copy;Training Centrum Den Haag 010 519 0532</article>
+            <article class="footer-subText">Developed by FastDevelopment 2017</article>
         </footer>
     </body>
 </html>
