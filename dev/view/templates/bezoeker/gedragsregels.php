1397d9336877b6ab259ed9810d3e54ed905c0ee9
GeertHeemskerk <leaguecrafterhd@gmail.com>
2017-04-06 11:13:10 +0200

    made gedragsregels for bezoekers

---
+++ dev/view/templates/bezoeker/gedragsregels.php
@@ -0,0 +1,19 @@
+<?php include VIEW_PATH . 'includes/header.php';?>
+<div class="content-holder">
+    <img src="img/boxing.jpg" class="content-img" />
+    <img src="img/boxing.jpg" class="content-img" />
+    <img src="img/boxing.jpg" class="content-img" />
+    <img src="img/boxing.jpg" class="content-img" />
+    <img src="img/boxing.jpg" class="content-img" />
+    <div class="registration-holder">
+        <h2>Gedragsregels</h2>
+        <p>Bij onze vereniging hanteren wij een aantal regels:</p>
+        <ul>
+            <li>Niet schelden.</li>
+            <li>Geen agressief gedrag tonen.</li>
+            <li>Optijd betalen.</li>
+            <li>Respect voor de instructeurs en onze regels.</li>  
+        </ul>
+    </div>
+</div>
+<?php include VIEW_PATH . 'includes/footer.php';?>
