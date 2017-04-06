4c48514c944d144a435b72fb5de38ec782949b20
Wouter van Kester <wouter_k_@hotmail.com>
2017-04-06 11:05:09 +0200

    adding homepage members

--- dev/view/templates/leden/default.php
+++ dev/view/templates/leden/default.php
@@ -1,1 +1,13 @@
-<?php
+<?php include VIEW_PATH . 'includes/header.php';?>
+<div class="content-holder">
+    <img src="img/boxing.jpg" class="content-img" />
+    <img src="img/boxing.jpg" class="content-img" />
+    <img src="img/boxing.jpg" class="content-img" />
+    <img src="img/boxing.jpg" class="content-img" />
+    <img src="img/boxing.jpg" class="content-img" />
+    <div class="text-holder">
+        <h2>Welkom bij Training Centrum Den Haag</h2>
+        <p>Den Haag Training Center is een sportschool waar onder profesionele begeleiding in een veilige omgeving verschillende soorten martial arts-, indoor bootcamp, personal- en small group trainingen worden aangeboden. Hier kan je je inschrijven op een les of uitschrijven op een les.</p>
+        
+</div>
+<?php include VIEW_PATH . 'includes/footer.php';?>
