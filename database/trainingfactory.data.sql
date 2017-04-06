9b7c3337a751d5fdb54c1d659557c749ac1a0b79
swu1337 <master_msp1@hotmail.com>
2017-04-06 14:47:10 +0200

    error in db fixed

--- database/trainingfactory.data.sql
+++ database/trainingfactory.data.sql
@@ -25,58 +25,19 @@ INSERT INTO `persons`
  `place`,
  `role`)
 VALUES
-(NULL,
-'member',
-'qwerty',
-'member',
-'van',
-'trainingfactory',
-'2017-04-04',
-'male',
-'member@member.com',
-NULL,
-NULL,
-'street1',
-'1234AB',
-'The Hague',
-'member'),
-(NULL,
-'instructor',
-'qwerty',
-'instructor',
-'van',
-'trainingfactory',
-'2017-03-04',
-'male',
-'instructor@instructor.com',
-'2018-02-04',
-'2500.50',
-'street1',
-'4234AC',
-'The Hague',
-'instructor'),
-(NULL,
-'admin',
-'qwerty',
-'admin',
-'van',
-'trainingfactory',
-'2017-04-05',
-'female',
-'admin@admin.com',
-'2018-05-08',
-'8000.50',
-'street1',
-'1111AB',
-'The Hague',
-'admin')
+(NULL,'member','qwerty','member','van','trainingfactory','2017-04-04','male','member@member.com',NULL,NULL,'street1','1234AB','The Hague','member'),
+(NULL,'instructor','qwerty','instructor','van','trainingfactory','2017-04-04','male','instructor@instructor.com','2013-04-04','2500.00','street2','2134AB','The Hague','instructor'),
+(NULL,'admin','qwerty','admin','van','trainingfactory','2013-04-04','female','admin@admin.com','2012-04-04','2500.00','street3','2134AC','The Hague','admin');
 
 --
 -- Data for Trainings
 --
 
-INSERT INTO `trainings` 
-(`id`, `description`, `duration`, `extra_costs`)
+INSERT INTO `trainings`
+(`id`,
+ `description`,
+ `duration`,
+ `extra_costs`)
 VALUES
 (NULL, 'Boksen', '01:20:00', NULL),
 (NULL, 'Kickboksen', '02:10:00', 10),
@@ -84,3 +45,7 @@ VALUES
 (NULL, 'Stootzak trainingen', '01:25:00', NULL),
 (NULL, 'Bootcamps', '03:30:00', 16.5),
 (NULL, 'Fitness-uren', '03:00:00', NULL);
+
+--
+-- Data for Lesssons
+--
