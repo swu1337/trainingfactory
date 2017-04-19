effb7860fa449085e2e33340ebdf7d1576b175dc
Steven Wu <master_msp1@hotmail.com>
2017-04-19 23:05:12 +0200

    removed unused function

--- dev/models/db/Lesson.php
+++ dev/models/db/Lesson.php
@@ -19,15 +19,5 @@ class Lesson extends Entiteit
         $this->id = filter_var($this->id, FILTER_VALIDATE_INT);
         $this->instructor_id = filter_var($this->instructor_id, FILTER_VALIDATE_INT);
         $this->training_id = filter_var($this->training_id, FILTER_VALIDATE_INT);
-        $this->db = new \PDO(DATA_SOURCE_NAME,DB_USERNAME,DB_PASSWORD);
-        $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-    }
-
-    public function getTraining() {
-        $sql = "SELECT * FROM `trainings` WHERE id = :id LIMIT 1";
-        $stmnt = $this->db->prepare($sql);
-        $stmnt->bindParam(':id', $this->id);
-        $stmnt->execute();
-        return $stmnt->fetchAll(\PDO::FETCH_CLASS, __NAMESPACE__ . '\Training');
     }
 }
